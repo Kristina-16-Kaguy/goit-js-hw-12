b5b712011ee47380bcb3325e93f02b7977faef57
Kristina-16-Kaguy <kristina.kaguy@gmail.com>
2025-09-30 19:21:10 +0000

    Deploying to gh-pages from @ Kristina-16-Kaguy/goit-js-hw-12@0e989e8cb388517919e3184a71ccc4e151d29ed9 🚀

---
+++ index.js
@@ -0,0 +1,24 @@
+import{i as c,a as f,S as m}from"./assets/vendor-DzBlEn4a.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))l(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const i of o.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&l(i)}).observe(document,{childList:!0,subtree:!0});function e(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function l(t){if(t.ep)return;t.ep=!0;const o=e(t);fetch(t.href,o)}})();function n(s){c.error({message:s,position:"topRight"})}const u="52515106-cf2fe0ed90e49660ada5f5535",d="https://pixabay.com/api/";function p(s){return f.get(d,h(s)).then(r=>{const e=r.data.hits;return(!e||!e.length)&&n("Sorry, there are no images matching your search query. Please try again!"),e}).catch(r=>(n(r.message),[]))}function h(s){return{params:{key:u,q:s,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:9}}}const a={form:document.querySelector(".form"),gallery:document.querySelector(".gallery"),loader:document.querySelector(".loader")},g=new m(".gallery a",{captionsData:"alt",captionDelay:250});function y(s){const r=s.map(e=>`<li class="gallery-item">
+          <a class="gallery-link" href="${e.largeImageURL}">
+            <img src="${e.webformatURL}" alt="${e.tags}" class="gallery-img">
+          </a>
+          <ul class="img-footer">
+            <li class="img-footer-item">
+              <h3 class="meta-header">Likes</h3>
+              <p class="meta-info">${e.likes}</p>
+            </li>
+            <li class="img-footer-item">
+              <h3 class="meta-header">Views</h3>
+              <p class="meta-info">${e.views}</p>
+            </li>
+            <li class="img-footer-item">
+              <h3 class="meta-header">Comments</h3>
+              <p class="meta-info">${e.comments}</p>
+            </li>
+            <li class="img-footer-item">
+              <h3 class="meta-header">Downloads</h3>
+              <p class="meta-info">${e.downloads}</p>
+            </li>
+           </ul>
+       </li>`).join("");a.gallery.innerHTML=r,g.refresh()}function L(){a.gallery.innerHTML=""}function b(){a.loader.classList.add("is-visible")}function w(){a.loader.classList.remove("is-visible")}a.form.addEventListener("submit",s=>{s.preventDefault();const r=s.target.elements["search-text"].value.trim();r&&(L(),b(),p(r).then(e=>y(e)).finally(w))});
+//# sourceMappingURL=index.js.map
