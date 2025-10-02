d8e6b54bfa9878dd65f6d20ebc95e3def59eabf1
Kristina-16-Kaguy <kristina.kaguy@gmail.com>
2025-10-02 14:31:45 +0300

    refactoring paxabay

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,24 +4,21 @@ import { showError } from './iziToastHelper.js';
 const API_KEY = '52515106-cf2fe0ed90e49660ada5f5535';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export function getImagesByQuery(query) {
-  return axios
-    .get(BASE_URL, getParams(query))
-    .then(response => {
-      const images = response.data.hits;
-
-      if (!images || !images.length) {
-        showError(
-          'Sorry, there are no images matching your search query. Please try again!'
-        );
-      }
-
-      return images;
-    })
-    .catch(error => {
-      showError(error.message);
+export async function getImagesByQuery(query) {
+  try {
+    let response = await axios.get(BASE_URL, getParams(query));
+    const images = response.data.hits;
+    if (!images || !images.length) {
+      showError(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
       return [];
-    });
+    }
+    return images;
+  } catch (error) {
+    showError(error.message);
+    return [];
+  }
 }
 
 function getParams(query) {
@@ -32,7 +29,7 @@ function getParams(query) {
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
-      per_page: 9,
+      per_page: 12,
     },
   };
 }
