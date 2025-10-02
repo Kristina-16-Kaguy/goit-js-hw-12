d1567d3440ce62743a592115d0fe3ff892298156
Kristina-16-Kaguy <kristina.kaguy@gmail.com>
2025-10-02 13:29:42 +0000

    Deploying to gh-pages from @ Kristina-16-Kaguy/goit-js-hw-12@1e865d955862dfeccd4b0dbf7e02b7ef913336fb 🚀

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-import{i as c,a as m,S as f}from"./assets/vendor-xwsNXkQR.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const a of t)if(a.type==="childList")for(const i of a.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function e(t){const a={};return t.integrity&&(a.integrity=t.integrity),t.referrerPolicy&&(a.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?a.credentials="include":t.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function n(t){if(t.ep)return;t.ep=!0;const a=e(t);fetch(t.href,a)}})();function l(r){c.error({message:r,position:"topRight"})}const u="52515106-cf2fe0ed90e49660ada5f5535",d="https://pixabay.com/api/";async function p(r){try{const e=(await m.get(d,g(r))).data.hits;return!e||!e.length?(l("Sorry, there are no images matching your search query. Please try again!"),[]):e}catch(s){return l(s.message),[]}}function g(r){return{params:{key:u,q:r,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:12}}}const o={form:document.querySelector(".form"),gallery:document.querySelector(".gallery"),loader:document.querySelector(".loader")},h=new f(".gallery a",{captionsData:"alt",captionDelay:250});function y(r){const s=r.map(e=>`<li class="gallery-item">
-          <a class="gallery-link" href="${e.largeImageURL}">
-            <img src="${e.webformatURL}" alt="${e.tags}" class="gallery-img">
+import{i as g,a as h,S as y}from"./assets/vendor-xwsNXkQR.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))o(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const c of s.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&o(c)}).observe(document,{childList:!0,subtree:!0});function a(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerPolicy&&(s.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?s.credentials="include":t.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function o(t){if(t.ep)return;t.ep=!0;const s=a(t);fetch(t.href,s)}})();function n(e){g.error({message:e,position:"topRight"})}const L="52515106-cf2fe0ed90e49660ada5f5535",w="https://pixabay.com/api/";async function m(e,r){try{const o=(await h.get(w,b(e,r))).data.hits;return!o||!o.length?(n("Sorry, there are no images matching your search query. Please try again!"),[]):o}catch(a){return n(a.message),[]}}function b(e,r){return{params:{key:L,q:e,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:15,page:r}}}const i={form:document.querySelector(".form"),gallery:document.querySelector(".gallery"),loader:document.querySelector(".loader"),LoadMoreBtn:document.querySelector(".load-more")},v=new y(".gallery a",{captionsData:"alt",captionDelay:250});function u(e){const r=e.map(a=>`<li class="gallery-item">
+          <a class="gallery-link" href="${a.largeImageURL}">
+            <img src="${a.webformatURL}" alt="${a.tags}" class="gallery-img">
           </a>
           <ul class="img-footer">
             <li class="img-footer-item">
               <h3 class="meta-header">Likes</h3>
-              <p class="meta-info">${e.likes}</p>
+              <p class="meta-info">${a.likes}</p>
             </li>
             <li class="img-footer-item">
               <h3 class="meta-header">Views</h3>
-              <p class="meta-info">${e.views}</p>
+              <p class="meta-info">${a.views}</p>
             </li>
             <li class="img-footer-item">
               <h3 class="meta-header">Comments</h3>
-              <p class="meta-info">${e.comments}</p>
+              <p class="meta-info">${a.comments}</p>
             </li>
             <li class="img-footer-item">
               <h3 class="meta-header">Downloads</h3>
-              <p class="meta-info">${e.downloads}</p>
+              <p class="meta-info">${a.downloads}</p>
             </li>
            </ul>
-       </li>`).join("");o.gallery.innerHTML=s,h.refresh()}function L(){o.gallery.innerHTML=""}function w(){o.loader.classList.add("is-visible")}function b(){o.loader.classList.remove("is-visible")}o.form.addEventListener("submit",async r=>{r.preventDefault();const s=r.target.elements["search-text"].value.trim();if(s){L(),w();try{let e=await p(s);y(e)}catch(e){l(e.message)}b()}});
+       </li>`).join("");i.gallery.insertAdjacentHTML("beforeend",r),v.refresh()}function S(){i.gallery.innerHTML=""}function f(){i.loader.classList.add("is-visible")}function d(){i.loader.classList.remove("is-visible")}let p=1,l;i.form.addEventListener("submit",async e=>{if(e.preventDefault(),l=e.target.elements["search-text"].value.trim(),!!l){S(),f();try{let r=await m(l,p);u(r)}catch(r){n(r.message)}d()}});i.LoadMoreBtn.addEventListener("click",async()=>{f();try{let e=await m(l,++p);u(e)}catch(e){n(e.message)}d()});
 //# sourceMappingURL=index.js.map
